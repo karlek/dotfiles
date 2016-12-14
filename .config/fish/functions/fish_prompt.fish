674b4e020f61bd3517dac4ac46440bc4f935890c
Henry Eklind <henrye@kth.se>
2016-12-14 01:08:01 +0100

    fish - hostname in prompt for less confusion

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -2,6 +2,7 @@ set __toaster_color_yellow E6DB7E
 set __toaster_color_pink F92672
 set __toaster_color_grey 554F48
 set __toaster_color_white AAAAAA
+set __toaster_color_blue 9999FF
 
 function __toaster_color_echo
 	set_color $argv[1]
@@ -22,6 +23,7 @@ function fish_prompt
 		__toaster_color_echo $__toaster_color_yellow "(^._.^)ﾉ"
 	end
 
+	__toaster_color_echo $__toaster_color_blue " "(hostname)
 	__toaster_color_echo $__toaster_color_grey " @ "
 	__toaster_color_echo $__toaster_color_pink (__toaster_current_folder)
 	__toaster_color_echo $__toaster_color_grey " \$ "
