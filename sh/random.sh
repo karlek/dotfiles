fd816684b4ce9eebb7065323997a91e84204294a
Henry Eklind <henrye@kth.se>
2017-01-30 12:49:06 +0100

    sh/random - change to fish

--- sh/random.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Get a random file from our wallpapers.
-files=(/home/_/Desktop/walls/ghostly/*)
-export FILE=$(printf "%s\n" "${files[RANDOM % ${#files[@]}]}")
-
-hsetroot -fill "$FILE" 2> /dev/null
-echo "$FILE" > /tmp/current-wall 
