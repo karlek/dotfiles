578e7f24bc4a79f88fa12a0a855d374a844249a9
Henry Eklind <henrye@kth.se>
2017-02-04 15:47:09 +0100

    fish - change path function to use fish_user_path specific env

--- .config/fish/conf.d/01-path.fish
+++ .config/fish/conf.d/01-path.fish
@@ -1,13 +1,8 @@
-function prepend_to_path -d "Prepend the given dir to PATH if it exists and is not already in it"
-    if test -d $argv[1]
-        if not contains $argv[1] $PATH
-            set -gx PATH "$argv[1]" $PATH
-        end
-    end
+function add_to_path -d "Add the given directory to user's path"
+        set -U fish_user_path $fish_user_path $argv
 end
-
-prepend_to_path $GOROOT/bin
-prepend_to_path $GOPATH/bin
-prepend_to_path ~/.local/bin
-prepend_to_path /usr/bin/core_perl
-prepend_to_path ~/sh
+add_to_path $GOROOT/bin
+add_to_path $GOPATH/bin
+add_to_path ~/.local/bin
+add_to_path /usr/bin/core_perl
+add_to_path ~/sh
