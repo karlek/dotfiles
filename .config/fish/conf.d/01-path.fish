2cd27b253d619c7d96b9a830a323edf5103eae3e
Henry Eklind <henrye@kth.se>
2017-02-04 21:10:06 +0100

    fish - fix typo of fish_user_pathS

--- .config/fish/conf.d/01-path.fish
+++ .config/fish/conf.d/01-path.fish
@@ -1,5 +1,5 @@
 function add_to_path -d "Add the given directory to user's path"
-        set -U fish_user_path $fish_user_path $argv
+        set -U fish_user_paths $fish_user_paths $argv
 end
 add_to_path $GOROOT/bin
 add_to_path $GOPATH/bin
