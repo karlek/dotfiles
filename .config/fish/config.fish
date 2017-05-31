9f887cf074104149a64caa406352a33106905935
Henry Eklind <henrye@kth.se>
2017-05-31 08:07:15 +0200

    fish - Specify tmux instance to statusline toggle

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,7 +8,7 @@ if status --is-login
 	nyfikend -v > /tmp/nyfiken 2> /tmp/nyfiken-error &
 	if not tmux has-session -t cmus 2> /dev/null
 		tmux new-session -s cmus -d -n cmus -d "/usr/bin/cmus" &
-		tmux set-option status off &
+		tmux set-option -t cmus status off &
 	end
 end
 
