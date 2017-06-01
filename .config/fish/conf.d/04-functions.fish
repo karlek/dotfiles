60d9d33f2bdba7f8bd790eae798e5a3bbfe90b26
Henry Eklind <henrye@kth.se>
2017-06-01 10:51:23 +0200

    fish - Shuffle around in functions.fish

--- .config/fish/conf.d/04-functions.fish
+++ .config/fish/conf.d/04-functions.fish
@@ -7,12 +7,9 @@ alias gotest='go test -v . | sed ''/PASS/s//(printf "\033[32mPASS\033[0m")/'' | 
 # Colorized cat (will guess file type based on contents)
 alias ccat 'pygmentize -g'
 
-# Sort packages by size
-alias pacbig="expac -H M '%m\t%n' | sort -h"
-alias pacexp='pacman -Qqe'
-
-# stdin to clipboard.
+# Stdin to clipboard.
 alias xin='xclip -in -selection clip'
+# Clipboard to stdout.
 alias xout='xclip -out'
 
 # Pong, reverse dns: `rdns <ip>` and my local ip.
@@ -20,60 +17,56 @@ alias pong='ping 8.8.8.8'
 alias rdns='dig +short -x'
 alias myip='ip route list | grep -o -P "src\s([0-9]{1,3}.){3}[0-9]{1,3}" | head -n1 | cut -c 5-'
 
-# Screen-saver
+# Screen-saver setting.
 alias no-ss='xset -dpms s off'
 alias ss='xset -dpms s on'
 
-# Date and battery
-alias a='grc a'
-alias c='cat'
-alias l='ls'
-alias r='rm'
-
-# Pacaur shortcuts
-abbr pa='pacaur'
-abbr pas='pacaur -Suy'
-abbr par='pacaur -Rns'
-abbr pss='pacaur -Ss'
-
-# Git shortcuts
-abbr gs='git status'
-abbr ga='git add'
-abbr gap='git add -p'
-abbr gc='git commit --verbose --gpg-sign'
-abbr gp='git push'
-abbr gd='git diff'
-abbr gl='git lg'
-abbr glp='git lg -p'
-abbr gdc='git diff --cached'
-
-alias tolower="tr '[:upper:]' '[:lower:]'"
-alias toupper="tr '[:lower:]' '[:upper:]'"
 alias suspend='systemctl suspend'
-
-# Shortcuts for acpi, date, etc.
-alias yt='youtube-dl'
 alias musb='mount /mnt'
 
-# Shortcut for ssh
+# Date and battery
+add_alias grc a 'grc a'
+
+# Sort packages by size
+alias pacbig="expac -H M '%m\t%n' | sort -h"
+# Export installed packages.
+alias pacexp='pacman -Qqe'
+
+# Pacaur shortcuts
+abbr pa  'pacaur'
+abbr pas 'pacaur -Suy'
+abbr par 'pacaur -Rns'
+abbr pss 'pacaur -Ss'
+
+# Git shortcuts
+abbr gs  'git status'
+abbr ga  'git add'
+abbr gap 'git add -p'
+abbr gc  'git commit --verbose --gpg-sign'
+abbr gp  'git push'
+abbr gd  'git diff'
+abbr gl  'git lg'
+abbr glp 'git lg -p'
+abbr gdc 'git diff --cached'
+
+alias yt='youtube-dl'
+
+# Shortcut for ssh.
 alias laputa='mosh laputa -- fish -c "tmux attach -d -t clean; or tmux"'
 
-# Volume
-alias vol='printf "%d%% and %s" (pamixer --get-volume) (pamixer --get-mute)'
+# Volume.
+alias vol='printf "%d%% and %s\n" (pamixer 0 --get-volume) (pamixer --get-mute | sed "s/true/mute/" | sed "s/false/voluble/")'
 
 # Screen cast.
 alias cast='ffmpeg -f x11grab -video_size 2560x1440 -i :0 -f alsa -i default -c:v ffvhuff -c:a flac test.mkv'
 
+# Binge watch, skip openings, endings and previews for all movies in the folder.
 function m
   if count $argv > /dev/null
     mpv --script=$XDG_CONFIG_HOME/mpv/skipchapters.lua $argv
   else
     mpv --script=$XDG_CONFIG_HOME/mpv/skipchapters.lua .
   end
-end
-
-function z
-  zathura $argv &; exit
 end
 
 # View images in current directory.
@@ -94,26 +87,8 @@ function duf
   end
 end
 
-# Sort by occurrences, use: `command | nums`
-function nums
-  awk '{!seen[$0]++};END{for(i in seen) print seen[i] " " i}' | sort -nr
-end
-
-function gcal
-  gcalcli --configFolder=$XDG_CONFIG_HOME/gcalcli --monday --military --nodetail_all -w (echo "$COLUMNS 23" | awk '{print int(($1/$2) * 3)}') calw
-end
-
-function nfiles
-  find . -maxdepth 1 -print0 | xargs -0 -I '{}' sh -c "printf \"%d {}\n\" \$(find '{}' -type f | wc -l)" | sort -n
-end
-
-function json
-  if count $argv > /dev/null
-    aeson-pretty 
-  else
-    aeson-pretty | pygmentize -l json -f terminal256
-  end
-end
+alias tolower="tr '[:upper:]' '[:lower:]'"
+alias toupper="tr '[:lower:]' '[:upper:]'"
 
 function oflen
   grep "^.\{$argv\}\$"
@@ -123,6 +98,38 @@ function urldecode
   php -r "echo urldecode('$argv');"
 end
 
+# Sort by occurrences, use: `command | nums`
+function nums
+  awk '{!seen[$0]++};END{for(i in seen) print seen[i] " " i}' | sort -nr
+end
+
+function nfiles
+  find . -maxdepth 1 -print0 | xargs -0 -I '{}' sh -c "printf \"%d {}\n\" \$(find '{}' -type f | wc -l)" | sort -n
+end
+
+function gcal
+  gcalcli --configFolder=$XDG_CONFIG_HOME/gcalcli --monday --military --nodetail_all -w (echo "$COLUMNS 23" | awk '{print int(($1/$2) * 3)}') calw
+end
+
+function json
+  if count $argv > /dev/null
+    aeson-pretty
+  else
+    aeson-pretty | pygmentize -l json -f terminal256
+  end
+end
+
 function imgcmp
   compare -metric RMSE $argv[1] $argv[2] NULL:
+end
+
+# Sublime text.
+add_alias subl3 subl 'subl3'
+
+alias irc='mosh laputa -- fish -c "tmux attach -d -t weechat; or tmux"'
+
+function unstaged
+  begin;
+    find . -type f | cut -c 3-; git ls-files;
+  end | sort | uniq -u
 end
