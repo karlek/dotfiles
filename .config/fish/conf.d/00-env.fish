a3a70274256b76c01f4cc4caa8f9a9db31c575ac
Henry Eklind <henrye@kth.se>
2017-06-01 10:42:52 +0200

    fish - Change GOROOT and align env
    I now use the arch linux go package instead of pulling the latest from
    golang.

--- .config/fish/conf.d/00-env.fish
+++ .config/fish/conf.d/00-env.fish
@@ -1,10 +1,9 @@
 # Set environment variables.
-set -x GOPATH "$HOME/go/privgo"
-set -x GOROOT "$HOME/go/go"
-set -x GOROOT_BOOTSTRAP "$HOME/go/go1.4"
-set -x EDITOR nvim
-set -x BROWSER firefox
-set -x STEAM_RUNTIME 1
+set -x GOPATH           "$HOME/go"
+set -x GOROOT           "/usr/lib/go"
+set -x EDITOR           "nvim"
+set -x BROWSER          "firefox"
+set -x STEAM_RUNTIME    1
 
 # Set locale.
 set -x LC_ALL "en_US.UTF-8"
