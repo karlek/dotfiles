f9da1fdca22dcb434a1d3dfdc6b0ea6bf1137794
Henry Eklind <henrye@kth.se>
2017-06-01 14:36:02 +0200

    Add install script for folder creation

---
+++ install.fish
@@ -0,0 +1,6 @@
+#!/usr/bin/fish
+mkdir "$XDG_RUNETIME_DIR/X" 2> /dev/null
+mkdir "$XDG_RUNETIME_DIR/urxvt" 2> /dev/null
+touch "$XDG_RUNETIME_DIR/X/authority" 2> /dev/null
+mkdir "$XDG_CACHE_HOME/i3" 2> /dev/null
+mkdir "$XDG_DATA_HOME/X" 2> /dev/null
