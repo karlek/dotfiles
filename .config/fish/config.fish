d06da65b223debe026c7c2c5d984ba7b47338f8b
Henry Eklind <henrye@kth.se>
2017-06-03 10:59:21 +0200

    fish - Move creation of temporary files to login

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,6 +5,11 @@ if test -d $autojump
 end
 
 if status --is-login
+	# Initialize runtime files.
+	mkdir "$XDG_RUNETIME_DIR/X" 2> /dev/null
+	mkdir "$XDG_RUNETIME_DIR/urxvt" 2> /dev/null
+	touch "$XDG_RUNETIME_DIR/X/authority" 2> /dev/null
+
 	nyfikend -v > /tmp/nyfiken 2> /tmp/nyfiken-error &
 	if not tmux has-session -t cmus 2> /dev/null
 		tmux new-session -s cmus -d -n cmus -d "/usr/bin/cmus" &
