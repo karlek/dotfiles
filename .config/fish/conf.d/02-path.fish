51b90fa3515e642567ca13e3031aea9108439e09
Henry Eklind <henrye@kth.se>
2017-06-04 13:54:58 +0200

    fish - Only add paths once
    Also removed old paths.

--- .config/fish/conf.d/02-path.fish
+++ .config/fish/conf.d/02-path.fish
@@ -1,3 +1,7 @@
+if contains fish_user_paths (set --names)
+	exit
+end
+
 function add_to_path -d "Add the given directory to user's path"
 	if test -d $argv
 		set -gx fish_user_paths $fish_user_paths $argv
@@ -6,6 +10,4 @@ end
 add_to_path $GOROOT/bin
 add_to_path $GOPATH/bin
 add_to_path ~/.local/bin
-add_to_path /usr/bin/core_perl
 add_to_path ~/sh
-add_to_path /home/_/nummen/matlab/bin
