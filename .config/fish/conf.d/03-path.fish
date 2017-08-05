cc933c14afdebd9b385592a683af1a4f80a8b6b4
Henry Eklind <henrye@kth.se>
2017-08-06 01:57:04 +0200

    fish - Add files missed in renaming

---
+++ .config/fish/conf.d/03-path.fish
@@ -0,0 +1,14 @@
+if contains fish_user_paths (set --names)
+	exit
+end
+
+function add_to_path -d "Add the given directory to user's path"
+	if test -d $argv
+		set -gx fish_user_paths $fish_user_paths $argv
+	end
+end
+add_to_path "$HOME/.local/bin"
+add_to_path "$HOME/sh"
+add_to_path "$GOROOT/bin"
+add_to_path "$GOPATH/bin"
+add_to_path "$XDG_DATA_HOME/cargo/bin"
