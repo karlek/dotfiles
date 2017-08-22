13a88d225fb0d68e28d9f7dba1544da36979eace
Henry Eklind <henry.eklind@tink.se>
2017-08-22 13:51:59 +0200

    fish/darwin - Add Firefox to path

--- .config/fish/conf.d/99-path.fish
+++ .config/fish/conf.d/99-path.fish
@@ -12,5 +12,6 @@ add_to_path $GOROOT/bin
 add_to_path $GOPATH/bin
 add_to_path /usr/local/opt/gettext/bin
 add_to_path /usr/local/bin
+add_to_path /Applications/FirefoxNightly.app/Contents/MacOS
 
 set -gx fish_user_paths_mac true
