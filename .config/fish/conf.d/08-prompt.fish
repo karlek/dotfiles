4dacbb490ef64a715880a433a3747cff1b4f7cb0
Henry Eklind <henry@karlek.io>
2018-10-19 10:32:33 +0200

    fish: Show active git branch in prompt

--- .config/fish/conf.d/08-prompt.fish
+++ .config/fish/conf.d/08-prompt.fish
@@ -3,6 +3,7 @@ set __toaster_color_pink F92672
 set __toaster_color_grey 554F48
 set __toaster_color_white AAAAAA
 set __toaster_color_blue 9999FF
+set __toaster_color_green 99FF99
 
 function __toaster_color_echo
 	set_color $argv[1]
@@ -11,6 +12,19 @@ end
 
 function __toaster_current_folder
 	echo -n $PWD
+end
+
+function is_git_folder
+    git rev-parse --abbrev-ref HEAD ^ /dev/null > /dev/null
+	if test $status -ne 0
+		echo 1
+		return
+	end
+	echo 0
+end
+
+function __toaster_git_branch
+	printf "(%s)" (git rev-parse --abbrev-ref HEAD ^ /dev/null)
 end
 
 function fish_prompt
@@ -25,6 +39,10 @@ function fish_prompt
 	__toaster_color_echo $__toaster_color_blue " "(hostname)
 	__toaster_color_echo $__toaster_color_grey " @ "
 	__toaster_color_echo $__toaster_color_pink (__toaster_current_folder)
+	if test (is_git_folder) -eq 0
+		__toaster_color_echo $__toaster_color_grey " : "
+		__toaster_color_echo $__toaster_color_green (__toaster_git_branch)
+	end
 	__toaster_color_echo $__toaster_color_grey " \$ "
 
 	# Fix, output color.
