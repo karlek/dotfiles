a34fe790413a5f8af1fe8c81ffb2f29bf82fbdb9
Henry Eklind <henry@karlek.io>
2018-11-21 11:38:32 +0100

    fish: Downgrade to firefox stable

--- .config/fish/conf.d/02-env.fish
+++ .config/fish/conf.d/02-env.fish
@@ -2,7 +2,7 @@
 set -x GOPATH           "$XDG_DATA_HOME/go"
 set -x GOROOT           "/usr/lib/go"
 set -x EDITOR           "nvim"
-set -x BROWSER          "firefox-nightly"
+set -x BROWSER          "firefox"
 set -x STEAM_RUNTIME    1
 
 # Set locale.
