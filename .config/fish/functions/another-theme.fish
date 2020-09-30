e4d38db5b39bf9ea027bbf821393904bae8f43fd
Henry Eklind <henry@karlek.io>
2020-09-30 11:58:12 +0200

    fish: Add another fish theme

---
+++ .config/fish/functions/another-theme.fish
@@ -0,0 +1,23 @@
+fish_color_autosuggestion '555'  'brblack'
+fish_color_cancel -r
+fish_color_command 005fd7
+fish_color_comment 990000
+fish_color_cwd green
+fish_color_cwd_root red
+fish_color_end 009900
+fish_color_error ff0000
+fish_color_escape 00a6b2
+fish_color_history_current --bold
+fish_color_host normal
+fish_color_host_remote yellow
+fish_color_match --background=brblue
+fish_color_normal normal
+fish_color_operator 00a6b2
+fish_color_param 00afff
+fish_color_quote 999900
+fish_color_redirection 00afff
+fish_color_search_match 'bryellow'  '--background=brblack'
+fish_color_selection 'white'  '--bold'  '--background=brblack'
+fish_color_status red
+fish_color_user brgreen
+fish_color_valid_path --underline
