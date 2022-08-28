9383fc37708fef56059b58d94f4292a087bf1fd6
Henry Eklind <henry@karlek.io>
2022-08-28 15:12:32 +0200

    fish: Start ssh-agent on start-up

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,11 @@
+if status is-login
+	set -x SSH_AUTH_SOCK /run/user/(id -u)/ssh-agent.$hostname.sock
+	ssh-add -l 2> /dev/null > /dev/null
+	if test $status -ge 2
+		ssh-agent -a "$SSH_AUTH_SOCK" > /dev/null
+	end
+end
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
