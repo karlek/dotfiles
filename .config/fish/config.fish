02b00e5f732f58c2484375e78ad8cf3ca80d7013
Henry Eklind <henry@karlek.io>
2024-10-18 07:42:07 +0200

    fish: remove unused binding

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -243,9 +243,6 @@ function fish_user_key_bindings
     # Alt+a : print the file (hidden included) contents of the current folder.
     bind \ea 'echo; and ls -a; and commandline -f repaint'
 
-    # Ctrl+delete: remove next word
-    bind \e\[3^ 'commandline -f kill-bigword; and commandline -f delete-char'
-
     # Alt+1 : pipe to in
     bind \e1 'commandline -i "| in"'
 
